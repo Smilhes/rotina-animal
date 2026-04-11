a41359c1f920b0e6599cc9d912a688bdcacf3157
diego99toff <42447455+diego99toff@users.noreply.github.com>
2026-04-11 10:40:29 +0000

    criação de cadastro  criação do cadastro cliente e cadastro pet

--- core/urls.py
+++ core/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.login_view, name='login'),
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('cadastro/', views.cadastro_cliente_view, name='cadastro_cliente'),
+    path('cadastro-pet/', views.cadastro_pet_view, name='cadastro_pet'),
     path('agendar/', views.agendamento_view, name='agendamento'),
     path('historico/', views.historico_view, name='historico'),
 ]
